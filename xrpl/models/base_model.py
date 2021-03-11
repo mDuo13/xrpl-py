03cf346ea59d0ec78241d0267ee057812f87fa46
Ted Kalaw <ted.kalaw@gmail.com>
2021-03-11 12:16:34 -0600

    Reverts comment

--- xrpl/models/base_model.py
+++ xrpl/models/base_model.py
@@ -18,7 +18,6 @@ REQUIRED: Final[object] = object()
 class BaseModel(ABC):
     """The base class for all model types."""
 
-    # Test
     def __init__(self: BaseModel, **kwargs: str) -> None:
         """Constructs a new BaseModel from a set of keyword arguments."""
         self.__dict__.update(kwargs)
